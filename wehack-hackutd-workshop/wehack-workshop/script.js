4d0e7c103fdd2962590777bbc47f812359c3fe6e
Swarita <71049346+Swarita@users.noreply.github.com>
2022-11-12 18:07:10 -0600

    Add files via upload

---
+++ wehack-hackutd-workshop/wehack-workshop/script.js
@@ -0,0 +1,23 @@
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
+
+
+// scroll to top functionality
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
